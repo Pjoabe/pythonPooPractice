c78cc148d3c2cc8f22985b182ce25ce56eb30b85
joabe <joabe9999pereira@gmail.com>
2023-06-20 14:52:37 -0300

    feat- busca por palavra

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = []
+    for data in instance.queue:
+        file_info = {
+            "palavra": word,
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for i, line in enumerate(data["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                file_info["ocorrencias"].append({"linha": i})
+        if file_info["ocorrencias"]:
+            search_result.append(file_info)
+    return search_result
 
 
 def search_by_word(word, instance):
