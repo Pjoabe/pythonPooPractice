0af9b732a956b66a3201f19bf1650affa1b01003
joabe <joabe9999pereira@gmail.com>
2023-06-20 13:18:00 -0300

    feat- requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.find(".txt") <= 0:
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    try:
+        with open(path_file, "r") as archive:
+            data = archive.read().split("\n")
+        return data
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
